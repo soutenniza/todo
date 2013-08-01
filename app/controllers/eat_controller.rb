fb885a51c8ae25b3b17bb705a2055221806e7c3b
Van Nguyen <soutennizavan@gmail.com>
2013-08-01 16:16:23 -0400

    added 'complex' stuff

--- app/controllers/eat_controller.rb
+++ app/controllers/eat_controller.rb
@@ -1,6 +1,12 @@
 class EatController < ApplicationController
   def food
     Resque.enqueue(Eat, params[:food])
+    i =0
+    while i < 210301239 do
+      i = 1+i
+    end
+
+    4000.times { 20*20*30 }
     render :text => "Put in fridge to eat later"
   end
 end
