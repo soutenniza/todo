eba36a5cd9c4e76641333c2987eab2c34691c812
Van Nguyen <soutennizavan@gmail.com>
2013-08-08 15:08:06 -0400

    fixed noob stuff

--- app/controllers/eat_controller.rb
+++ app/controllers/eat_controller.rb
@@ -3,12 +3,7 @@ class EatController < ApplicationController
     Resque.enqueue(Eat, params[:food])
     i =0
     while i < 2109301239 do
-      if i % 2 = 0
-        i = i + 2
-      end
-      if i % 3
-        i = i - 1
-      end
+      i = i + 1 
     end
 
     4000.times { 20*20*30 }
