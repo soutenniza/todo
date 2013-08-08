e494abf39f5df123c77e4599990f3759617d4662
Van Nguyen <soutennizavan@gmail.com>
2013-08-08 16:06:52 -0400

    hel

--- app/controllers/eat_controller.rb
+++ app/controllers/eat_controller.rb
@@ -2,7 +2,7 @@ class EatController < ApplicationController
   def food
     Resque.enqueue(Eat, params[:food])
     i =0
-    while i < 2109301239 do
+    while i < 21301239 do
       i = i + 1 
     end
 
